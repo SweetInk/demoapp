d8de9d79b70c53286e1b43a1dcfc2155e8fe68d4
SUCHU <danjicf@gmail.com>
2016-02-01 10:43:47 +0800

    Initial commit

---
+++ app/src/main/java/com/huashukang/demoapp/BedListAcitivity.java
@@ -0,0 +1,13 @@
+package com.huashukang.demoapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BedListAcitivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bed_list_acitivity);
+    }
+}
